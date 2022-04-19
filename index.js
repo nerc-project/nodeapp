9d0c4b6b8253541b35368f89680d1b7dd4c5b53e
Milstein Munakami <milsonmun@gmail.com>
2022-04-18 21:23:11 -0400

    Test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From NERC!" }');
+    res.send('{ "response": "Hello From NERC Prod!" }');
 });
 
 app.get('/will', function (req, res) {
